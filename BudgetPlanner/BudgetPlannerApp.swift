58a6e8a8f6b0a378e6dbb4dfd1dd55ef3f36b982
Nadeera Sampath <nadeera3784@gmail.com>
2023-09-11 23:43:00 +0530

    Initial Commit

---
+++ BudgetPlanner/BudgetPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetPlannerApp.swift
+//  BudgetPlanner
+//
+//  Created by Nadeera Sampath on 2023-09-11.
+//
+
+import SwiftUI
+
+@main
+struct BudgetPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
